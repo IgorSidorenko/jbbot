c5314dc4b0f7e0da7bbb8322988d201b6862af80
igorsid <sidgarri@gmail.com>
2025-01-21 15:50:49 +0300

    refactoring

--- talkingbot.py
+++ talkingbot.py
@@ -5,20 +5,15 @@ greetings = ["Hello", "Hi", "How are you", "Greeting"]
 chat_invitations = ["What do you want to talk about?", "You wanna talk?", "What about a small chat?", "Let's talk!"]
 responses = ["You are right", "Yes", "I agree"]
 
-jokes = {"bird": "birds are living in the Ocean",
-         "fish": "fishes are living on trees",
-         "human": "humans are always jumping",
-         "animal": "every animal can speak English"
-         }
-
+jokes = {
+    "bird": "birds are living in the Ocean",
+    "fish": "fishes are living on trees",
+    "human": "humans are always jumping",
+    "animal": "every animal can speak English"
+}
 
 chatbot_name = "Mia"
 chatbot_prefix = chatbot_name + ": "
-
-print(chatbot_prefix + random.choice(greetings) + "! " + "What's you name? ")
-user_name = input("User: ")
-
-user_prefix = user_name + ": "
 
 # Functions
 def chatbot_phrase(phrase):
@@ -29,7 +24,6 @@ def user_phrase():
 
 def chat():
     print(chatbot_phrase(random.choice(chat_invitations)))
-
     user_replica = user_phrase()
 
     if user_replica == "Bye":
@@ -37,16 +31,21 @@ def chat():
     else:
         was_kidding = False
         for key_word in jokes.keys():
-            if user_replica.find(key_word) != -1:
+            if key_word in user_replica:
                 print(chatbot_phrase(jokes[key_word]))
                 was_kidding = True
                 break
 
-        if was_kidding == False:
-            print(chatbot_phrase(random.choice(responses) + ", " + user_name + ", " + user_replica))
-
+        if not was_kidding:
+            print(chatbot_phrase(f"{random.choice(responses)}, {user_name}, {user_replica}"))
         chat()
 
+def main():
+    print(chatbot_phrase(random.choice(greetings)) + "! What's your name?")
+    global user_name, user_prefix
+    user_name = input("User: ")
+    user_prefix = user_name + ": "
+    chat()
 
-#Main
-chat() # YOUR PROGRAM STARTS RUNNING FROM HERE!
+if __name__ == "__main__":
+    main()
